64c7f8988d7aa3a3d9cb8214d1ad47f441a29a7f
Tom Holohan <thomasholohan8@gmail.com>
2021-10-11 12:46:28 +0000

    Create booking page and form

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
 
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(elems);
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.timepicker');
+  var instances = M.Timepicker.init(elems);
+});
+
+$('#textarea1').val('New Text');
+  M.textareaAutoResize($('#textarea1'));

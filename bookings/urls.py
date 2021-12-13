3ef023abbdc55bf730d9d4ced9ec1413a9a1a51c
Tom Holohan <thomasholohan8@gmail.com>
2021-12-13 12:16:49 +0000

    Update color palette

--- bookings/urls.py
+++ bookings/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.returnHome, name='home'),
     path('submit/', views.submit_page, name='submit'),
     path('about/', views.returnAboutPage, name='about'),
-    path('appointments', views.return_appointments, name='appointments'),
+    path('submissions', views.return_appointments, name='submissions'),
 ]
